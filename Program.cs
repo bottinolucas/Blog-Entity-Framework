d9a77731653261d6f66e653dfdfc2181c3cefe95
bottinolucas <lucasgabottino@gmail.com>
2024-09-20 14:53:33 -0300

    CRUD basico usando DbContext

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BlogEF.Data;
+using BlogEF.Models;
+using Microsoft.Extensions.Primitives;
+
+using (var ctx = new BlogDataContext())
+{
+  //CREATE 
+  //Exemplo: Criar uma nova Tag
+  // var tag = new Tag {
+  //   Name = "ASPNET",
+  //   Slug = "aspnet"
+  // };
+  // ctx.Tags.Add(tag);
+  // ctx.SaveChanges();
+
+  //UPDATE
+  //Exemplo: Alterar uma Tag (Rehydration)
+  // var tag = ctx.Tags.FirstOrDefault(x => x.Id == 1);
+  // tag.Name = ".NET";
+  // tag.Slug = "dotnet";
+  // ctx.Update(tag);
+  // ctx.SaveChanges();
+
+  //DELETE
+  //Exemplo: Remover uma Tag 
+  var tag = ctx.Tags.FirstOrDefault(x => x.Id == 1);
+  ctx.Remove(tag);
+  ctx.SaveChanges();
+}
