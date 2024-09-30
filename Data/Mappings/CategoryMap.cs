aa858538af77b8c4c4b0dc65e562bf3c25031adb
bottinolucas <lucasgabottino@gmail.com>
2024-09-30 17:20:04 -0300

    Adicionando propriedades

--- Data/Mappings/CategoryMap.cs
+++ Data/Mappings/CategoryMap.cs
@@ -16,7 +16,18 @@ namespace BlogEF.Data.Mappings
         builder.Property(x => x.Id)
           .ValueGeneratedOnAdd()
           .UseIdentityColumn(); //PRIMARY KEY IDENTITY(1,1)
+        //Propriedades
+        builder.Property(x => x.Name)
+          .IsRequired() //NOT NULL
+          .HasColumnName("Name")
+          .HasColumnType("NVARCHAR")
+          .HasMaxLength(80);
 
+        builder.Property(x => x.Slug)
+          .IsRequired() //NOT NULL
+          .HasColumnName("Slug")
+          .HasColumnType("VARCHAR")
+          .HasMaxLength(80);
       }
     }
 }
