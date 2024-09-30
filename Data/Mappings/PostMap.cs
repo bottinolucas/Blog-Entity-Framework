3723d6e86f79d55a8b04e4df6365db2ed987db64
bottinolucas <lucasgabottino@gmail.com>
2024-09-30 19:24:55 -0300

    Relacionamento Muitos para Muitos no PostMap

--- Data/Mappings/PostMap.cs
+++ Data/Mappings/PostMap.cs
@@ -33,6 +33,22 @@ namespace BlogEF.Data.Mappings
             .WithMany(x => x.Posts)
             .HasConstraintName("FK_Post_Category")
             .OnDelete(DeleteBehavior.Cascade);
+
+          builder.HasMany(x => x.Tags)
+            .WithMany(x => x.Posts)
+            .UsingEntity<Dictionary<string, object>>(
+              "PostTag",
+              post => post.HasOne<Tag>()
+                .WithMany()
+                .HasForeignKey("PostId")
+                .HasConstraintName("FK_PostTag_PostId")
+                .OnDelete(DeleteBehavior.Cascade),
+              tag => tag.HasOne<Post>()
+                .WithMany()
+                .HasForeignKey("TagId")
+                .HasConstraintName("FK_PostTag_TagId")
+                .OnDelete(DeleteBehavior.Cascade)
+            );
         }
     }
 }
