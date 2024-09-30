f148e13d9ce7fb7a0d92ac311776bd0a79ea9d5d
bottinolucas <lucasgabottino@gmail.com>
2024-09-30 19:53:08 -0300

    Relacionamento muitos para muitos em UserMap, usando mapeamentos criados e testando relacionamentos

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -1,3 +1,4 @@
+using BlogEF.Data.Mappings;
 using BlogEF.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,7 +7,17 @@ namespace BlogEF.Data
     //Para ser um Data Context, deve herdar de DbContext
     public class BlogDataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }  
+        public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=34358873Lucas;TrustServerCertificate=True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+        }
     }   
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using BlogEF.Data;
+using BlogEF.Models;
+
+using var context = new BlogDataContext();
+// context.Users.Add(new User
+//   {
+//     Bio = "Software",
+//     Email = "lucasgabottino@gmail.com",
+//     Image = "https://balta.io",
+//     Name = "Lucas Bottino",
+//     PasswordHash = "1q2w3e4r5t6y",
+//     Slug = "software"
+//   }
+// );
+// context.SaveChanges();
+
+var user = context.Users.FirstOrDefault();
+
+var post = new Post
+{
+  Author = user,
+  Body = "article",
+  Category = new Category 
+  {
+    Name = "backend",
+    Slug = "some backend"
+  },
+  CreateDate = DateTime.Now,
+  Slug = "some text",
+  Summary = "Some summary",
+  Title = "my article"
+};
+
+context.Posts.Add(post);
+context.SaveChanges();
