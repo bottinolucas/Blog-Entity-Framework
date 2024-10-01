7e6f0002bef5b6a4d65f2907ecb9978c14380404
bottinolucas <lucasgabottino@gmail.com>
2024-10-01 08:20:57 -0300

    Corrigindo erros e adicionando readme

--- Program.cs
+++ Program.cs
@@ -2,34 +2,35 @@
 using BlogEF.Models;
 
 using var context = new BlogDataContext();
-// context.Users.Add(new User
-//   {
-//     Bio = "Software",
-//     Email = "lucasgabottino@gmail.com",
-//     Image = "https://balta.io",
-//     Name = "Lucas Bottino",
-//     PasswordHash = "1q2w3e4r5t6y",
-//     Slug = "software"
-//   }
-// );
-// context.SaveChanges();
-
-var user = context.Users.FirstOrDefault();
-
-var post = new Post
-{
-  Author = user,
-  Body = "article",
-  Category = new Category 
+context.Users.Add(new User
   {
-    Name = "backend",
-    Slug = "some backend"
-  },
-  CreateDate = DateTime.Now,
-  Slug = "some text",
-  Summary = "Some summary",
-  Title = "my article"
-};
-
-context.Posts.Add(post);
+    Bio = "Software",
+    Email = "lucasgabottino@gmail.com",
+    Image = "https://balta.io",
+    Name = "Lucas Bottino",
+    PasswordHash = "1q2w3e4r5t6y",
+    Slug = "software",
+    GitHub = "bottinolucas"
+  }
+);
 context.SaveChanges();
+
+// var user = context.Users.FirstOrDefault();
+
+// var post = new Post
+// {
+//   Author = user,
+//   Body = "article",
+//   Category = new Category 
+//   {
+//     Name = "backend",
+//     Slug = "some backend"
+//   },
+//   CreateDate = DateTime.Now,
+//   Slug = "some text",
+//   Summary = "Some summary",
+//   Title = "my article"
+// };
+
+// context.Posts.Add(post);
+// context.SaveChanges();
---
+++ README.md
@@ -0,0 +1,3 @@
+## Para criar uma migracao, `dotnet ef migrations add NomeMigracao`
+
+## O comando `dotnet ef database update` cria o banco de dados, mapeando pelo Fluent Mapping
